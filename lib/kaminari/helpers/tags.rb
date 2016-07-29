160a56d6d412ca76384f3e1d91ebea37f5cc0994
Maicon Sanson <maiconsanson@gmail.com>
2016-07-29 11:06:22 -0300

    no message

--- lib/kaminari/helpers/tags.rb
+++ lib/kaminari/helpers/tags.rb
@@ -50,7 +50,7 @@ module Kaminari
         #   #     to: {other: "params", user: {name: "yuki", page: nil}}
         #   #   (when @param_name == "user[page]")
         #   # @param_name.to_s.scan(/[\w\.]+/)[0..-2].inject(page_params){|h, k| h[k] }[$&] = nil
-          @param_name.scan(/[\w\.]+/)[0..-2].inject(page_params){|h, k| h.merge![k] }[$&] = nil
+          @param_name.to_a.scan(/[\w\.]+/)[0..-2].inject(page_params){|h, k| h.merge![k] }[$&] = nil
         # end
 
         page_params
